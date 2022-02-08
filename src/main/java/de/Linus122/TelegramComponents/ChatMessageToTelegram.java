5dee2150a8dd22f1391cf1746cc2f71a1b09ac65
iCaitlyn <55845572+iCaitlyn@users.noreply.github.com>
2022-02-09 01:23:23 +0800

    Update ChatMessageToTelegram.java

--- src/main/java/de/Linus122/TelegramComponents/ChatMessageToTelegram.java
+++ src/main/java/de/Linus122/TelegramComponents/ChatMessageToTelegram.java
@@ -2,6 +2,6 @@ package de.Linus122.TelegramComponents;
 
 public class ChatMessageToTelegram extends Cancellable {
 	public String text;
-	public int chat_id;
+	public long chat_id;
 	public String parse_mode;
 }
