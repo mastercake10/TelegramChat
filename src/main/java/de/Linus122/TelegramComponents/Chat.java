17ed5b5859718de5b13b9630878d7285f84f990f
iCaitlyn <55845572+iCaitlyn@users.noreply.github.com>
2022-02-09 01:22:39 +0800

    Update Chat.java

--- src/main/java/de/Linus122/TelegramComponents/Chat.java
+++ src/main/java/de/Linus122/TelegramComponents/Chat.java
@@ -1,14 +1,14 @@
 package de.Linus122.TelegramComponents;
 
 public class Chat {
-	private int id;
+	private long id;
 	private String type;
 
-	public int getId() {
+	public long getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
